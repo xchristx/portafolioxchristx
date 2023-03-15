bd3305199cb3c493fbfbfab72115d40582058ace
xchristx <christian.aguilar.dev@gmail.com>
2023-03-15 11:40:23 -0400

    cv

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,126 +1,283 @@
-import React, { useLayoutEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Box, Button, CardMedia, Divider, Grid, Typography,Stack, IconButton, Badge } from '@mui/material';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import FilePresentIcon from '@mui/icons-material/FilePresent';
-import { gsap,Power3 } from 'gsap';
-import contact from '../assets/contact.png';
-import cvLogo from '../assets/cv.png';
-import './AboutBackGroud.scss';
-import '../index.css';
+import React, { useLayoutEffect } from "react";
+import { useLocation } from "react-router-dom";
+import {
+  Box,
+  Button,
+  CardMedia,
+  Divider,
+  Grid,
+  Typography,
+  Stack,
+  IconButton,
+  Badge,
+} from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import FilePresentIcon from "@mui/icons-material/FilePresent";
+import { gsap, Power3 } from "gsap";
+import contact from "../assets/contact.png";
+import cvLogo from "../assets/cv.png";
+import "./AboutBackGroud.scss";
+import "../index.css";
 
-export const Contact = ()=>{
-    const tl = gsap.timeline();
+export const Contact = () => {
+  const tl = gsap.timeline();
 
-    const location = useLocation();
+  const location = useLocation();
 
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      if (location.hash == "#contact") {
+        tl.from(".imageContact", {
+          opacity: 0,
+          duration: 1,
+          x: 1000,
+          scaleX: 0,
+          scaleY: 0,
+          stagger: 0.3,
+          ease: Power3.easeOut,
+        })
+          .from(
+            ".contentContact",
+            {
+              x: -100,
+              y: "2rem",
+              opacity: 0,
+              duration: 1,
+              stagger: 0.1,
+              ease: Power3.easeIn,
+            },
+            "-=1.7"
+          )
+          .from(
+            ".iconsContact",
+            {
+              y: 800,
+              opacity: 0.5,
+              duration: 1,
+              stagger: 0.3,
+              ease: Power3.easeIn,
+            },
+            "-=1"
+          );
+      } else {
+        tl.to(".imageContact", {
+          opacity: 0,
+          duration: 1,
+          x: 100,
+          stagger: 0.3,
+          ease: Power3.easeOut,
+        })
+          .to(
+            ".contentContact",
+            {
+              x: "2rem",
+              opacity: 0,
+              duration: 1,
+              stagger: 0.1,
+              ease: Power3.easeIn,
+            },
+            "-=1.5"
+          )
+          .to(
+            ".iconsContact",
+            {
+              y: 800,
+              opacity: 0,
+              duration: 1,
+              stagger: 0.1,
+              ease: Power3.easeIn,
+            },
+            "-=1.5"
+          );
+      }
+    }, root);
 
-    useLayoutEffect(() => {
-        let ctx = gsap.context(() => {
-            if (location.hash == '#contact'){
-                tl.from('.imageContact', { opacity: 0, duration:1, x:1000,  scaleX:0, scaleY:0,  stagger:0.3, ease: Power3.easeOut })
-                .from('.contentContact', {x:-100, y: '2rem', opacity: 0,duration:1, stagger:0.1, ease: Power3.easeIn }, '-=1.7')
-                .from('.iconsContact', {y:800, opacity: 0.5,duration:1, stagger:0.3, ease: Power3.easeIn }, '-=1')
-            }else {
-                tl.to('.imageContact', { opacity: 0, duration:1, x:100, stagger:0.3, ease: Power3.easeOut })
-                .to('.contentContact', { x: '2rem', opacity: 0,duration:1, stagger:0.1, ease: Power3.easeIn }, '-=1.5')
-                .to('.iconsContact', { y: 800, opacity: 0,duration:1, stagger:0.1, ease: Power3.easeIn }, '-=1.5');
-            }
-        }, root);
+    return () => ctx.revert();
+  }, [location.hash]);
 
-        return () => ctx.revert();
-    }, [location.hash])
-
-    return(
-        <>
-            <div className="stars"></div>
-            <div className="twinkling"></div>
-            <Box component='div' sx={{ display:{xs:'none', sm:'flex'}, position:'static',}} >
-                <Stack sx={{position:'absolute', zIndex:30, top:'20%', left:'5%', }} >
-
-                    <Typography variant='body1'
-                        className='contentContact'
-                        sx={{ color:'#FF4D5A ', letterSpacing:10, fontSize: { xs:'40px', md:'80px'}, fontWeight:500,mb:'5%' }} >
-                        GET IN TOUCH
-                    </Typography>
-                    <Box sx={{width:'50%'}} >
-                        <hr className="bookends contentContact" />
-                    </Box>
-                    <Button sx={{justifyContent:'flex-start', mr:60, mt:'5%'}} href='mailto:christian.aguilar.dev@gmail.com' >
-                      <Typography variant='body1'
-                          className='contentContact'
-                          sx={{ color:'#fff ',letterSpacing:3, fontSize: { xs:'10px', sm:'25px'}, fontWeight:500}} >
-                          christian.aguilar.dev@gmail.com
-                      </Typography>
-                    </Button>
-                    <Stack direction='row' >
-                      <IconButton className='iconsContact' href='https://www.linkedin.com/in/christian-aguilar-dev/' target="_blank" >
-                        <LinkedInIcon sx={{color:'white', p:0}} fontSize='large' />
-                      </IconButton>
-                      <IconButton className='iconsContact' href='https://github.com/xchristx' target="_blank" >
-                        <GitHubIcon  sx={{color:'white', p:0}} fontSize='large' />
-                      </IconButton>
-                      <IconButton className='iconsContact' href='https://drive.google.com/file/d/1cNVOcY9J6GtwaMLruY8jwTluDGfjbLWJ/view?usp=sharing' target="_blank" >
-                        <Badge sx={{maxWidth:35, maxHeight:35,}} >
-                            <img src={cvLogo} />
-                        </Badge>
-                      </IconButton>
-                    </Stack>
-                </Stack>
-                <Box sx={{ width:'50%', height:'60%', top:'20%', position: 'absolute',  zIndex:20, right:{xs:30,md:'5%'},  }} >
-                        <CardMedia
-                                component='img'
-                                className='imageContact'
-                                src={contact}
-                                sx={{ boxShadow:7, height:'100%',borderRadius:1 }}
-
-                            />
-                </Box>
-            </Box>
-            <Box component='div' sx={{ display:{xs:'flex', sm:'none'}, position:'static',}} >
-
-                <Box sx={{ width:'80%', height:'40%', top:'20%', position: 'absolute',  zIndex:20, left:'50%', transform: 'translateX(-50%)',p:0,m:0 }} >
-                    <CardMedia
-                            component='img'
-                            className='imageContact'
-                            src={contact}
-                            sx={{ boxShadow:20, height:'100%',  }}
-
-                        />
-                </Box>
-                <Stack sx={{ width:'80%', position:'absolute', zIndex:30, left:'50%', transform: 'translateX(-50%)', bottom:'17%' }} >
-                    <Typography variant='body1'
-                        className='contentContact'
-                        sx={{ color:'#FF4D5A ', letterSpacing:4, fontSize: '29px', fontWeight:500, }} >
-                        GET IN TOUCH
-                    </Typography>
-                    <Button sx={{justifyContent:'flex-start',}} href='mailto:christian.aguilar.dev@gmail.com' >
-                        <Typography variant='body1'
-                            className='contentContact'
-                            sx={{ color:'#fff ',letterSpacing:1, textTransform:'lowercase', fontSize: '17px', fontWeight:500}}
-                        >
-                            christian.aguilar.dev@gmail.com
-                        </Typography>
-                    </Button>
-                    <Stack direction='row' >
-                      <IconButton href='https://www.linkedin.com/in/christian-aguilar-dev/' target="_blank" >
-                        <LinkedInIcon className='iconsContact' sx={{color:'white', p:0}} fontSize='large' />
-                      </IconButton>
-                      <IconButton href='https://github.com/xchristx' target="_blank" >
-                        <GitHubIcon className='iconsContact' sx={{color:'white', p:0}} fontSize='large' />
-                      </IconButton>
-                      <IconButton className='iconsContact' href='https://drive.google.com/file/d/1cNVOcY9J6GtwaMLruY8jwTluDGfjbLWJ/view?usp=sharing' target="_blank" >
-                        <Badge sx={{maxWidth:35, maxHeight:35}} >
-                            <img src={cvLogo} />
-                        </Badge>
-                      </IconButton>
-                    </Stack>
-                </Stack>
-
-            </Box>
-
-        </>
-    )
-
-}
+  return (
+    <>
+      <div className="stars"></div>
+      <div className="twinkling"></div>
+      <Box
+        component="div"
+        sx={{ display: { xs: "none", sm: "flex" }, position: "static" }}
+      >
+        <Stack
+          sx={{ position: "absolute", zIndex: 30, top: "20%", left: "5%" }}
+        >
+          <Typography
+            variant="body1"
+            className="contentContact"
+            sx={{
+              color: "#FF4D5A ",
+              letterSpacing: 10,
+              fontSize: { xs: "40px", md: "80px" },
+              fontWeight: 500,
+              mb: "5%",
+            }}
+          >
+            GET IN TOUCH
+          </Typography>
+          <Box sx={{ width: "50%" }}>
+            <hr className="bookends contentContact" />
+          </Box>
+          <Button
+            sx={{ justifyContent: "flex-start", mr: 60, mt: "5%" }}
+            href="mailto:christian.aguilar.dev@gmail.com"
+          >
+            <Typography
+              variant="body1"
+              className="contentContact"
+              sx={{
+                color: "#fff ",
+                letterSpacing: 3,
+                fontSize: { xs: "10px", sm: "25px" },
+                fontWeight: 500,
+              }}
+            >
+              christian.aguilar.dev@gmail.com
+            </Typography>
+          </Button>
+          <Stack direction="row">
+            <IconButton
+              className="iconsContact"
+              href="https://www.linkedin.com/in/christian-aguilar-dev/"
+              target="_blank"
+            >
+              <LinkedInIcon sx={{ color: "white", p: 0 }} fontSize="large" />
+            </IconButton>
+            <IconButton
+              className="iconsContact"
+              href="https://github.com/xchristx"
+              target="_blank"
+            >
+              <GitHubIcon sx={{ color: "white", p: 0 }} fontSize="large" />
+            </IconButton>
+            <IconButton
+              className="iconsContact"
+              href="https://drive.google.com/file/d/1s3-AT_90Nct3IbzK4ps9ydvPItk45vch/view?usp=sharing"
+              target="_blank"
+            >
+              <Badge sx={{ maxWidth: 35, maxHeight: 35 }}>
+                <img src={cvLogo} />
+              </Badge>
+            </IconButton>
+          </Stack>
+        </Stack>
+        <Box
+          sx={{
+            width: "50%",
+            height: "60%",
+            top: "20%",
+            position: "absolute",
+            zIndex: 20,
+            right: { xs: 30, md: "5%" },
+          }}
+        >
+          <CardMedia
+            component="img"
+            className="imageContact"
+            src={contact}
+            sx={{ boxShadow: 7, height: "100%", borderRadius: 1 }}
+          />
+        </Box>
+      </Box>
+      <Box
+        component="div"
+        sx={{ display: { xs: "flex", sm: "none" }, position: "static" }}
+      >
+        <Box
+          sx={{
+            width: "80%",
+            height: "40%",
+            top: "20%",
+            position: "absolute",
+            zIndex: 20,
+            left: "50%",
+            transform: "translateX(-50%)",
+            p: 0,
+            m: 0,
+          }}
+        >
+          <CardMedia
+            component="img"
+            className="imageContact"
+            src={contact}
+            sx={{ boxShadow: 20, height: "100%" }}
+          />
+        </Box>
+        <Stack
+          sx={{
+            width: "80%",
+            position: "absolute",
+            zIndex: 30,
+            left: "50%",
+            transform: "translateX(-50%)",
+            bottom: "17%",
+          }}
+        >
+          <Typography
+            variant="body1"
+            className="contentContact"
+            sx={{
+              color: "#FF4D5A ",
+              letterSpacing: 4,
+              fontSize: "29px",
+              fontWeight: 500,
+            }}
+          >
+            GET IN TOUCH
+          </Typography>
+          <Button
+            sx={{ justifyContent: "flex-start" }}
+            href="mailto:christian.aguilar.dev@gmail.com"
+          >
+            <Typography
+              variant="body1"
+              className="contentContact"
+              sx={{
+                color: "#fff ",
+                letterSpacing: 1,
+                textTransform: "lowercase",
+                fontSize: "17px",
+                fontWeight: 500,
+              }}
+            >
+              christian.aguilar.dev@gmail.com
+            </Typography>
+          </Button>
+          <Stack direction="row">
+            <IconButton
+              href="https://www.linkedin.com/in/christian-aguilar-dev/"
+              target="_blank"
+            >
+              <LinkedInIcon
+                className="iconsContact"
+                sx={{ color: "white", p: 0 }}
+                fontSize="large"
+              />
+            </IconButton>
+            <IconButton href="https://github.com/xchristx" target="_blank">
+              <GitHubIcon
+                className="iconsContact"
+                sx={{ color: "white", p: 0 }}
+                fontSize="large"
+              />
+            </IconButton>
+            <IconButton
+              className="iconsContact"
+              href="https://drive.google.com/file/d/1cNVOcY9J6GtwaMLruY8jwTluDGfjbLWJ/view?usp=sharing"
+              target="_blank"
+            >
+              <Badge sx={{ maxWidth: 35, maxHeight: 35 }}>
+                <img src={cvLogo} />
+              </Badge>
+            </IconButton>
+          </Stack>
+        </Stack>
+      </Box>
+    </>
+  );
+};
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,89 +1,198 @@
-import React, { useLayoutEffect, } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Box, Divider, Grid, IconButton, Typography, Stack, Badge } from '@mui/material';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import { gsap,Power3 } from 'gsap';
-import { HomeBackGround } from './HomeBackGround';
-import cvLogo from '../assets/cv.png';
-import '/animate.css'
+import React, { useLayoutEffect } from "react";
+import { useLocation } from "react-router-dom";
+import {
+  Box,
+  Divider,
+  Grid,
+  IconButton,
+  Typography,
+  Stack,
+  Badge,
+} from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { gsap, Power3 } from "gsap";
+import { HomeBackGround } from "./HomeBackGround";
+import cvLogo from "../assets/cv.png";
+import "/animate.css";
 
-export const Home = ()=>{
-    
-    const tl = gsap.timeline();
+export const Home = () => {
+  const tl = gsap.timeline();
 
-    const location = useLocation()
-    
-    useLayoutEffect(() => {
-        let ctx = gsap.context(() => {
-            if (location.hash == '#home'){
-                tl.from('.titleLeft', { opacity: 0, duration:1, x:-1000, stagger:0.3, ease: Power3.easeOut })
-                .from('.titleRight', { opacity: 0, duration:1, x:1000, stagger:0.1, ease: Power3.easeIn }, '-=1.4')
-                .from('.content', { y: '2rem', opacity: 0,duration:0.5, stagger:0.3, ease: Power3.easeIn }, '-=1.5');
-            }else{
-                tl.to('.titleLeft', { opacity: 0, duration:0.7, x:-100, stagger:0.3, ease: Power3.easeOut })
-                .to('.titleRight', { opacity: 0, duration:0.7, x:100, stagger:0.3, ease: Power3.easeIn }, '-=1.3')
-                .to('.content', { y: '2rem', opacity: 0.3,duration:0.6, stagger:0.3, ease: Power3.easeIn }, '-=1.3');
-            }
-        }, root); 
-    
-        return () => ctx.revert();
-    }, [location.hash])
-    
+  const location = useLocation();
 
-    return(
-        <>
-            <HomeBackGround  sx={{position:'absolute'}} />
-            <Grid container sx={{ height:'100vh', display: 'flex', textAlign:'center',  alignItems:'center', justifyContent:'center'}}  >
-               
-                <Grid item xs={12} md={6} sx={{zIndex:100}}  >
-                    <Typography 
-                     className='titleLeft'
-                     sx={{fontSize: { xs:'30px',sm:'40px', md:'80px'}, color:'#FF4D5A',fontWeight:500, position:{xs:'absolute', md:'relative'}, top:{xs:40, sm:85, md:0}, left:{xs:'50%', md:0}, transform: {xs:'translateX(-50%)', md: 'none'}  }} 
-                    >
-                        PORTFOLIO
-                    </Typography>
-                </Grid>
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      if (location.hash == "#home") {
+        tl.from(".titleLeft", {
+          opacity: 0,
+          duration: 1,
+          x: -1000,
+          stagger: 0.3,
+          ease: Power3.easeOut,
+        })
+          .from(
+            ".titleRight",
+            {
+              opacity: 0,
+              duration: 1,
+              x: 1000,
+              stagger: 0.1,
+              ease: Power3.easeIn,
+            },
+            "-=1.4"
+          )
+          .from(
+            ".content",
+            {
+              y: "2rem",
+              opacity: 0,
+              duration: 0.5,
+              stagger: 0.3,
+              ease: Power3.easeIn,
+            },
+            "-=1.5"
+          );
+      } else {
+        tl.to(".titleLeft", {
+          opacity: 0,
+          duration: 0.7,
+          x: -100,
+          stagger: 0.3,
+          ease: Power3.easeOut,
+        })
+          .to(
+            ".titleRight",
+            {
+              opacity: 0,
+              duration: 0.7,
+              x: 100,
+              stagger: 0.3,
+              ease: Power3.easeIn,
+            },
+            "-=1.3"
+          )
+          .to(
+            ".content",
+            {
+              y: "2rem",
+              opacity: 0.3,
+              duration: 0.6,
+              stagger: 0.3,
+              ease: Power3.easeIn,
+            },
+            "-=1.3"
+          );
+      }
+    }, root);
 
-                <Grid item xs={12} md={6} sx={{zIndex:100, mt:{ xs:'-90%', sm:-25, md:0 }}}  >
-                    <Stack>
-                        <Typography variant='body1' 
-                        className='titleRight'
-                        sx={{ textAlign:'start', pl:{ xs:3, sm:8, md:9 }, fontWeight:500, fontSize: { xs:'40px', sm:'55px', md:'5.1rem'}, letterSpacing:{ xs:5, md:19}, }} >
-                            CHRISTIAN
-                        </Typography>
-                        <Typography variant='title' 
-                            className='titleRight'
-                            sx={{ textAlign:'start', pl:{ xs:3, sm:8, md:9 }, fontWeight:500, fontSize: { xs:'35px', sm:'55px', md:'4.5rem'}, letterSpacing:{ xs:5, md:19}, mb:{xs:'5%', sm:'5%'} }} >
-                            AGUILAR
-                        </Typography>
-                        <hr className="bookends content" />
-                        <Typography variant='body2' 
-                            className='content'
-                            sx={{ pl:{ xs:3, sm:8, md:9 }, textAlign:'start',fontSize:'1.5rem',letterSpacing:2.5, mt:{xs:'5%', sm:'5%'} }} >
-                            Front End Developer
-                        </Typography>
-                    </Stack>
+    return () => ctx.revert();
+  }, [location.hash]);
 
-                </Grid>
+  return (
+    <>
+      <HomeBackGround sx={{ position: "absolute" }} />
+      <Grid
+        container
+        sx={{
+          height: "100vh",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Grid item xs={12} md={6} sx={{ zIndex: 100 }}>
+          <Typography
+            className="titleLeft"
+            sx={{
+              fontSize: { xs: "30px", sm: "40px", md: "80px" },
+              color: "#FF4D5A",
+              fontWeight: 500,
+              position: { xs: "absolute", md: "relative" },
+              top: { xs: 40, sm: 85, md: 0 },
+              left: { xs: "50%", md: 0 },
+              transform: { xs: "translateX(-50%)", md: "none" },
+            }}
+          >
+            PORTFOLIO
+          </Typography>
+        </Grid>
 
-                    <Stack direction='row' sx={{zIndex:100, position: 'absolute', bottom:40,  right:27  }}  >
-                        <IconButton  href='https://www.linkedin.com/in/christian-aguilar-dev/' target="_blank" >
-                            <LinkedInIcon sx={{color:'white', p:0,}} fontSize='large' />
-                        </IconButton>
-                        <IconButton  href='https://github.com/xchristx' target="_blank" >
-                            <GitHubIcon  sx={{color:'white', p:0}} fontSize='large' />
-                        </IconButton>
-                        <IconButton  href='https://drive.google.com/file/d/1cNVOcY9J6GtwaMLruY8jwTluDGfjbLWJ/view?usp=sharing' target="_blank" >
-                            <Badge sx={{maxWidth:35, maxHeight:35}} >
-                                <img src={cvLogo} />
-                            </Badge>
-                        </IconButton>
-                    </Stack>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          sx={{ zIndex: 100, mt: { xs: "-90%", sm: -25, md: 0 } }}
+        >
+          <Stack>
+            <Typography
+              variant="body1"
+              className="titleRight"
+              sx={{
+                textAlign: "start",
+                pl: { xs: 3, sm: 8, md: 9 },
+                fontWeight: 500,
+                fontSize: { xs: "40px", sm: "55px", md: "5.1rem" },
+                letterSpacing: { xs: 5, md: 19 },
+              }}
+            >
+              CHRISTIAN
+            </Typography>
+            <Typography
+              variant="title"
+              className="titleRight"
+              sx={{
+                textAlign: "start",
+                pl: { xs: 3, sm: 8, md: 9 },
+                fontWeight: 500,
+                fontSize: { xs: "35px", sm: "55px", md: "4.5rem" },
+                letterSpacing: { xs: 5, md: 19 },
+                mb: { xs: "5%", sm: "5%" },
+              }}
+            >
+              AGUILAR
+            </Typography>
+            <hr className="bookends content" />
+            <Typography
+              variant="body2"
+              className="content"
+              sx={{
+                pl: { xs: 3, sm: 8, md: 9 },
+                textAlign: "start",
+                fontSize: "1.5rem",
+                letterSpacing: 2.5,
+                mt: { xs: "5%", sm: "5%" },
+              }}
+            >
+              Front End Developer
+            </Typography>
+          </Stack>
+        </Grid>
 
-
-            </Grid>
-        </>
-    )
-    
-}
+        <Stack
+          direction="row"
+          sx={{ zIndex: 100, position: "absolute", bottom: 40, right: 27 }}
+        >
+          <IconButton
+            href="https://www.linkedin.com/in/christian-aguilar-dev/"
+            target="_blank"
+          >
+            <LinkedInIcon sx={{ color: "white", p: 0 }} fontSize="large" />
+          </IconButton>
+          <IconButton href="https://github.com/xchristx" target="_blank">
+            <GitHubIcon sx={{ color: "white", p: 0 }} fontSize="large" />
+          </IconButton>
+          <IconButton
+            href="https://drive.google.com/file/d/1s3-AT_90Nct3IbzK4ps9ydvPItk45vch/view?usp=sharing"
+            target="_blank"
+          >
+            <Badge sx={{ maxWidth: 35, maxHeight: 35 }}>
+              <img src={cvLogo} />
+            </Badge>
+          </IconButton>
+        </Stack>
+      </Grid>
+    </>
+  );
+};
